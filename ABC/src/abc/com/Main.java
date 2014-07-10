0bf7e5c62edcdaa7c77bdca93117bef5909ce20c
Thanh DInh <dhthanh@gmail.com>
2014-07-10 14:44:51 +0700

    first version

---
+++ ABC/src/abc/com/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package abc.com;
+
+/**
+ * @author thanhdh
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
